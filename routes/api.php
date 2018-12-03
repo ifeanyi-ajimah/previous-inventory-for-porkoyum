7e0b71d882fbe8e753d9cf733c6c3cc39a214159
peter-okafor <peter.okafor@kiakiafx.com>
2018-12-03 10:20:55 +0100

    test webhook 1

--- routes/api.php
+++ routes/api.php
@@ -42,4 +42,4 @@ Route::post('orders/autodoctor', 'OrderController@autoDoctor');
 Route::post('orders/mark-as-delivered', 'OrderController@markAsDelivered');
 Route::post('orders/mark-as-pending', 'OrderController@markAsPending');
 
-Route::get('orders/create', 'OrderController@apiStore');
+Route::post('orders/create', 'OrderController@apiStore');
