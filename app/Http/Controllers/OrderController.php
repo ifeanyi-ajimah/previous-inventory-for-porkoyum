53dab45bcfbd92f7714805f8d34c9a61a24db486
peter-okafor <peter.okafor@kiakiafx.com>
2018-12-04 13:33:29 +0100

    test webhook 22

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -39,25 +39,14 @@ class OrderController extends Controller
         // );
         // $endpoint = 'orders/2114';
         // $request = $woocommerce->get($endpoint);
-        $onlineCustomerName = $request->billing->first_name.' '.$request->billing->last_name;
-        $customer = Customer::where('name',$onlineCustomerName)->first();
-        if (!$customer) {
-            //customers not in the database
-            $onlineCustomerPhone = $request->billing->phone;
-
-            $onlineCustomerAddress = $request->billing->address_1.', '.($request->billing->address_2!=''?$request->billing->address_2.', ':'').$this->stateCode($request->billing->state)->name;
 
             $newCustomer = new Customer;
-            $newCustomer->name = $onlineCustomerName;
-            $newCustomer->address = $onlineCustomerAddress;
-            $newCustomer->phone_no = $onlineCustomerPhone;
-            $newCustomer->url = $website_url;
+            $newCustomer->name = '$onlineCustomerName';
+            $newCustomer->address = '$onlineCustomerAddress';
+            $newCustomer->phone_no = '$onlineCustomerPhone';
+            $newCustomer->url = '$website_url';
             $newCustomer->save();
 
-            $online['customer_id'] = $newCustomer->id;
-        } else {
-            $online['customer_id'] = $customer->id;
-        }
         return response()->json(['message' => 'Order created'],200);
     }
 
